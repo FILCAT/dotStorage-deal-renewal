9e2d9705688986259e28208e800a0b257457d8c1
Mike Seiler <snissn@gmail.com>
2023-03-06 19:21:16 -1000

    python code to deploy and interact with contract

---
+++ renew.py
@@ -0,0 +1,85 @@
+from web3 import Web3
+import uuid
+import time
+import os
+import json
+w3 = Web3(Web3.HTTPProvider('https://api.hyperspace.node.glif.io/rpc/v1'))
+abi_json = "out/DealClientStorageRenewal.sol/DealClientStorageRenewal.json"
+try:
+    abi = json.load(open(abi_json))['abi']
+    bytecode = json.load(open(abi_json))['bytecode']['object']
+except Exception:
+    print("Run forge b to compile abi")
+    raise
+
+PA=w3.eth.account.from_key(os.environ['PRIVATE_KEY'])
+
+address = "0x625Bd54897F23ceB99114F3d1acD439A4515C6F6"
+curBlock = w3.eth.get_block('latest')
+print(curBlock)
+
+
+
+def getTxInfo():
+    return { 'from': PA.address,
+            'nonce': w3.eth.get_transaction_count(PA.address)}
+
+def sendTx(tx):
+    tx_create = w3.eth.account.sign_transaction(tx, PA.privateKey)
+    tx_hash = w3.eth.send_raw_transaction(tx_create.rawTransaction)
+    return w3.eth.wait_for_transaction_receipt(tx_hash)
+
+
+def deploy():
+    DealClientStorageRenewal = w3.eth.contract(abi=abi, bytecode=bytecode)
+    tx_info = getTxInfo()
+    construct_txn = DealClientStorageRenewal.constructor().buildTransaction(tx_info)
+    tx_receipt = sendTx(construct_txn)
+    print(f'Contract deployed at address: { tx_receipt.contractAddress }')
+
+
+def getContract():
+    ContractFactory = w3.eth.contract(abi=abi)
+    contract = ContractFactory(address)
+    return contract
+
+def isVerified(actorId):
+    contract = getContract()
+    return contract.functions.isVerifiedSP(actorId).call()
+
+
+
+def testVerified():
+    is_v = isVerified(5)
+    assert(is_v)
+    is_v = isVerified(234234)
+    assert(not is_v)
+
+
+def wait(blockNumber):
+    wait_block_count = 0
+    while True:
+        curBlock = w3.eth.get_block('latest')
+        if curBlock.number - blockNumber > wait_block_count:
+            return
+        time.sleep(1)
+    
+def testAddRandomSP():
+    actor_id = uuid.uuid1().int>>64
+    print("Creating new actor", actor_id)
+    is_v = isVerified(actor_id)
+    assert(not is_v)
+    print("Actor is not verified. Adding actor to verfied SP list")
+
+    contract = getContract()
+    tx_info = getTxInfo()
+    tx_receipt = sendTx(contract.functions.addVerifiedSP(actor_id).buildTransaction(tx_info))
+    print("wait for confirmations")
+    wait(tx_receipt.blockNumber)
+    print("test the new actor is verified")
+    is_v = isVerified(actor_id)
+    assert(is_v)
+
+testAddRandomSP()
+
+#deploy()
